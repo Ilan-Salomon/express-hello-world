0804905a2628c92b3b0d7f37c3e627741557dbe3
Ilan Salomon <ilansalomon@gmail.com>
2025-06-19 16:10:18 -0400

    third commit added a health endpoint

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+// Add a new route for the health endpoint 
+
+app.get("/health", (req, res) => { 
+
+  res.sendStatus(200); 
+
+});
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
@@ -18,8 +26,6 @@ const html = `
     <script>
       setTimeout(() => {
         confetti({
-          particleCount: 100,
-          spread: 70,
           origin: { y: 0.6 },
           disableForReducedMotion: true
         });
